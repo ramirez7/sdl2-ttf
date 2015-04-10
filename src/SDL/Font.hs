841145e52853ddbad32920b0d33530a8551495dc
sbidin <sinisa@bidin.cc>
2015-04-10 19:35:15 +0200

    Add loading fonts from files and freeing them.

--- src/SDL/Font.hs
+++ src/SDL/Font.hs
@@ -20,25 +20,31 @@ High-level bindings to the @SDL_ttf@ library.
 module SDL.Font where
 
 import Control.Exception      (bracket, throwIO)
+import Control.Monad          (unless)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Bits              ((.|.))
 import Data.ByteString        (ByteString)
 import Data.ByteString.Unsafe (unsafeUseAsCStringLen)
 import Data.Data              (Data)
 import Data.List              (find)
-import Data.Text              (pack)
+import Data.Text              (Text, pack)
+import Data.Text.Foreign      (lengthWord16, unsafeCopyToPtr)
 import Data.Typeable          (Typeable)
+import Data.Word              (Word8)
 import Foreign.C.String       (withCString)
-import Foreign.C.Types        (CInt)
-import Foreign.Ptr            (Ptr, castPtr)
-import Foreign.Storable       (peek)
+import Foreign.C.Types        (CInt, CUShort)
+import Foreign.Marshal.Alloc  (allocaBytes, alloca)
+import Foreign.Marshal.Utils  (with)
+import Foreign.Ptr            (Ptr, castPtr, nullPtr)
+import Foreign.Storable       (peek, poke, pokeByteOff)
 import GHC.Generics           (Generic)
+import Linear                 (V4(..))
 import SDL                    (Renderer, Texture, Surface(..))
 import SDL.Exception          (SDLException(..), throwIfNull, throwIfNeg_)
 import SDL.Raw.Filesystem     (rwFromFile, rwFromConstMem)
+import SDL.Raw.Font           (PointSize)
 import SDL.Raw.Types          (RWops)
 import System.IO.Unsafe       (unsafePerformIO)
-import Control.Monad (unless)
 
 import qualified SDL.Raw
 import qualified SDL.Raw.Font
@@ -61,3 +67,18 @@ initialize = do
 -- call 'initialize' again before using any other parts of the library.
 quit :: MonadIO m => m ()
 quit = SDL.Raw.Font.quit
+
+-- | Represents a loaded font.
+newtype Font = Font { fontPtr :: Ptr SDL.Raw.Font.Font }
+
+-- | Loads a file for use as a 'Font', at a certain 'PointSize'. Works with
+-- @TTF@ and @FON@ files.
+load :: MonadIO m => FilePath -> PointSize -> m Font
+load path size = do
+  fmap Font .
+    throwIfNull "SDL.Font.load" "TTF_OpenFont" .
+      liftIO . withCString path $ flip SDL.Raw.Font.openFont size
+
+-- | Frees a loaded 'Font'.
+freeFont :: MonadIO m => Font -> m ()
+freeFont = SDL.Raw.Font.closeFont . fontPtr
