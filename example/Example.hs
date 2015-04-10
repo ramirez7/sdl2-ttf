127031c6dae7de5ae7104395a168109bd80f0278
sbidin <sinisa@bidin.cc>
2015-04-10 19:38:32 +0200

    Add a simple example executable.

--- example/Example.hs
+++ example/Example.hs
@@ -1,10 +1,56 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Control.Concurrent (threadDelay)
+import Data.Text          (Text)
+import Data.Text.IO       (putStrLn)
+import Linear             (V4(..))
+import Prelude     hiding (putStrLn)
+import System.Environment (getArgs)
+import System.Exit        (exitFailure)
 
 import qualified SDL
 import qualified SDL.Font
 
+red :: SDL.Font.Color
+red = V4 maxBound minBound minBound maxBound
+
+-- A sequence of example actions to be perfomed and displayed.
+examples :: [(Text, SDL.Window -> FilePath -> IO ())]
+examples = [
+  ("Loading font, blitting some text",
+    \window path -> do
+      font   <- SDL.Font.load path 50
+      screen <- SDL.getWindowSurface window
+      text   <- SDL.Font.renderBlended font red "Why hello there!"
+      SDL.Font.freeFont font
+      SDL.blitSurface text Nothing screen Nothing
+      SDL.freeSurface text
+      SDL.updateWindowSurface window)]
+
 main :: IO ()
 main = do
+
+  SDL.initialize [SDL.InitVideo]
   SDL.Font.initialize
-  print =<< SDL.Font.version
+
+  getArgs >>= \case
+
+    [] -> do
+      putStrLn "Usage: cabal run path/to/font.(ttf|fon)"
+      exitFailure
+
+    -- Run each of the examples within a newly-created window.
+    (path:_) -> do
+      flip mapM_ examples $ \(name, action) -> do
+        putStrLn name
+        window <- SDL.createWindow name SDL.defaultWindow
+        SDL.showWindow window
+        action window path
+        threadDelay 1000000
+        SDL.destroyWindow window
+
   SDL.Font.quit
+  SDL.quit
